47705735a30873d2e3e4c5484d6371b3d65f6ae5
Karl <karllamoureux2@gmail.com>
2020-06-01 07:46:21 -0400

    Implement ObjectSorter.readObjectFile()

--- src/main/objectSorter/ObjectSorter.java
+++ src/main/objectSorter/ObjectSorter.java
@@ -2,6 +2,50 @@ package main.objectSorter;
 
 import java.io.*;
 
-public class ObjectSorter {
+public class ObjectSorter <T> {
+	
+	private T exampleType;
+	
+	public ObjectSorter(T ex) {
+		exampleType = ex;
+	}
+	
+	/**
+	 * Reads object file, returns Object if instanceof T, null otherwise
+	 * @param file
+	 * @return
+	 */
+	public T readObjectFile(File file){
+		FileInputStream fis;
+		ObjectInputStream ois;
+		try {
+			fis = new FileInputStream(file);
+			ois = new ObjectInputStream(fis);
+			
+			Object object = ois.readObject();
+			
+			if(object.getClass() == this.exampleType.getClass()) {
+				fis.close();
+				ois.close();
+				return (T)object;
+			}
+			else {
+				fis.close();
+				ois.close();
+				return null;
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		return null;
+	}
+	
+	
 
 }
