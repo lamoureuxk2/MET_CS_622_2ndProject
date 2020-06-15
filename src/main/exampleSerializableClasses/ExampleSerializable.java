6cd7796f712fc471099ed4ac7f08ae26ccdf040e
Karl <karllamoureux2@gmail.com>
2020-06-15 10:23:27 -0400

    Edit test instance generator to make many instances of 5 different objects

--- src/main/exampleSerializableClasses/ExampleSerializable.java
+++ src/main/exampleSerializableClasses/ExampleSerializable.java
@@ -4,4 +4,5 @@ import java.io.Serializable;
 
 public interface ExampleSerializable extends Serializable {
 
+
 }
--- src/main/exampleSerializableClasses/TestClass1InstanceGenerator.java
+++ src/main/exampleSerializableClasses/TestClass1InstanceGenerator.java
@@ -7,16 +7,15 @@ import java.io.ObjectOutputStream;
 public class TestClass1InstanceGenerator {
 	
 	public static final String EXAMPLE_OBJECT_DIRECTORY = "RandomObjectFolderForTesting";
-	public static final int AMOUNT_OF_OBJECTS_TO_GENERATE = 10;
+	public static final int AMOUNT_OF_OBJECTS_TO_GENERATE = 100;
 
 	public static void main(String[] args) {
-		TestClass1 testObj = new TestClass1();
-		TestClass1[] testObjArr = testObjectArray();
+		ExampleSerializable[] testObjArr = testObjectArray();
 		try {
+			int count = 0;
 			
-			
-			for(TestClass1 obj : testObjArr) {
-				FileOutputStream fs = new FileOutputStream(EXAMPLE_OBJECT_DIRECTORY + "/example"+ obj.data1 +".obj");
+			for(ExampleSerializable obj : testObjArr) {
+				FileOutputStream fs = new FileOutputStream(EXAMPLE_OBJECT_DIRECTORY + "/example"+ count++ +".obj");
 				ObjectOutputStream os = new ObjectOutputStream(fs);
 				os.writeObject(obj);
 				fs.close();
@@ -30,10 +29,25 @@ public class TestClass1InstanceGenerator {
 
 	}
 	
-	public static TestClass1[] testObjectArray() {
-		TestClass1[] array = new TestClass1[AMOUNT_OF_OBJECTS_TO_GENERATE];
-		for(int i = 0; i < array.length; i++) {
-			array[i] = new TestClass1(i);
+	public static ExampleSerializable[] testObjectArray() {
+		ExampleSerializable[] array = new ExampleSerializable[AMOUNT_OF_OBJECTS_TO_GENERATE];
+		for(int i = 0, cycle = 1; i < array.length; i++, cycle++) {
+			if(cycle <= 1) {
+				array[i] = new TestClass1(i);
+			}
+			else if( cycle == 2 ) {
+				array[i] = new TestClass2(i);
+			}
+			else if( cycle == 3 ) {
+				array[i] = new TestClass3(i);
+			}
+			else if( cycle == 4 ) {
+				array[i] = new TestClass4(i);
+			}
+			else {
+				array[i] = new TestClass5(i);
+				cycle = 0;
+			}
 		}
 		return array;
 	}
